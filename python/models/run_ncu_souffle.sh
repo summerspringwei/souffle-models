74316bbae272a90ba4dc664aa89deb4c6ea54607
Chunwei Xia <120237776@qq.com>
2023-10-17 08:05:13 +0800

    Fixed the errors

--- python/models/run_ncu_souffle.sh
+++ python/models/run_ncu_souffle.sh
@@ -1,29 +1,31 @@
-set -xe
+#!/bin/bash
+
+set -x
 DIRPATH=/workspace/souffle-models/python/models
 
 cd ${DIRPATH}/bert
 # BERT Pass
-if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
-  ncu --clock-control none --set full -o ncu-souffle_bert_O4 -f --target-processes all python3 souffle_bert.py O4 1 1
-  ncu -i ./ncu-souffle_bert_O4.ncu-rep --csv --page raw  | grep -v "at::native*" | grep -v "at_cuda_detail" > ncu-souffle_bert_O4.csv
-fi
-SOUFFLE_BERT_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py ncu-souffle_bert_O4.csv)
-bert_layer=12
-SOUFFLE_BERT_LATENCY=$(python3 -c "print(${SOUFFLE_BERT_LATENCY} * ${bert_layer})")
+# if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
+#   ncu --clock-control none --set detailed -o ncu-souffle_bert_O4 -f --target-processes all python3 souffle_bert.py O4 1 1
+#   ncu -i ./ncu-souffle_bert_O4.ncu-rep --csv --page raw  | grep -v "at::native*" | grep -v "at_cuda_detail" > ncu-souffle_bert_O4.csv
+# fi
+# SOUFFLE_BERT_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py ncu-souffle_bert_O4.csv)
+# bert_layer=12
+# SOUFFLE_BERT_LATENCY=$(python3 -c "print(${SOUFFLE_BERT_LATENCY} * ${bert_layer})")
 
-# ResNext Pass
-cd ${DIRPATH}/resnext
-if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
-ncu --clock-control none --set full -o ncu-souffle_resnext_O4 -f --target-processes all \
-  python3 run_souffle_resnext.py O2 1 1 | tee resnext_O4_ncu.txt 2>&1
-ncu -i ./ncu-souffle_resnext_O4.ncu-rep --csv --page raw  | grep -v "at::native*" > ncu-souffle_resnext_O4.csv
-fi
-SOUFFLE_RESNEXT_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py ncu-souffle_resnext_O4.csv)
+# # ResNext Pass
+# cd ${DIRPATH}/resnext
+# if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
+# ncu --clock-control none --set detailed -o ncu-souffle_resnext_O4 -f --target-processes all \
+#   python3 run_souffle_resnext.py O2 1 1 | tee resnext_O4_ncu.txt 2>&1
+# ncu -i ./ncu-souffle_resnext_O4.ncu-rep --csv --page raw  | grep -v "at::native*" > ncu-souffle_resnext_O4.csv
+# fi
+# SOUFFLE_RESNEXT_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py ncu-souffle_resnext_O4.csv)
 
 # LSTM Pass
 cd ${DIRPATH}/lstm
 if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
-ncu --clock-control none --set full -o ncu-souffle_lstm_O4 -f --target-processes all \
+ncu --clock-control none --set detailed -o ncu-souffle_lstm_O4 -f --target-processes all \
  python3 souffle_lstm.py O3 1 1
 ncu -i ./ncu-souffle_lstm_O4.ncu-rep --csv --page raw  | grep -v "at::native*" > ncu-souffle_lstm_O4.csv
 fi
@@ -48,10 +50,11 @@ SOUFFLE_SWIN_TRANS_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py nc
 # MMOE
 cd ${DIRPATH}/mmoe
 if [ -n "${SOUFFLE_RUN}" ] && [ "${SOUFFLE_RUN}" = "TRUE" ]; then
-ncu --clock-control none --set full -o ncu-souffle_mmoe_O2 -f --target-processes all python3 souffle_mmoe.py O2 1 1
+ncu --clock-control none --set detailed -o ncu-souffle_mmoe_O2 -f --target-processes all python3 souffle_mmoe.py O2 1 1
 ncu -i ./ncu-souffle_mmoe_O2.ncu-rep --csv --page raw  | grep -v "at::native*" > ncu-souffle_mmoe_O2.csv
 fi
 SOUFFLE_MMoE_LATENCY=$(python3 ../../extract_ncu_cuda_kernel_latency.py ncu-souffle_mmoe_O2.csv)
+cd ${DIRPATH}
 
 echo "Souffle: ," ${SOUFFLE_BERT_LATENCY}, ${SOUFFLE_RESNEXT_LATENCY}, \
   ${SOUFFLE_LSTM_LATENCY}, ${SOUFFLE_EFFICIENTNET_LATENCY}, \
